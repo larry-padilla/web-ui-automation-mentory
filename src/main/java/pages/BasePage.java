9031013aab532cc0894a3ec44710d558a9815949
EstebanGlober <esteban.acuna@globant.com>
2023-07-19 10:27:54 -0500

    Create HomeLumaPage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class BasePage {
-    public WebDriver driver;
-    public Actions action;
+
+    protected WebDriver driver;
+    protected Actions action;
     private By singInLink = By.className("authorization-link");
     private By welcomeMessage = By.className("logged-in");
     private By menOption = By.className("level0 nav-3 category-item level-top parent ui-menu-item");
@@ -16,24 +17,26 @@ public class BasePage {
     public BasePage (WebDriver driver,Actions action) {
         this.driver = driver;
         this.action = action;
-
     }
 
     public void clickSingIn(){
         driver.findElement(singInLink).click();
     }
+
     public void hoverMenOption(){
         WebElement menOption = driver.findElement(By.className("level0 nav-3 category-item level-top parent ui-menu-item"));
         action.moveToElement(menOption).perform();
     }
+
     public void hoverTopsOption(){
-        Actions action = new Actions(driver);
         WebElement topsOption = driver.findElement(By.id("ui-id-17"));
         action.moveToElement(topsOption).perform();
     }
+
     public String getWelcomeMessage(){
         return driver.findElement(welcomeMessage).getText();
     }
+
     public void clickJacketsOption(){
         driver.findElement(jacketOption).click();
     }
